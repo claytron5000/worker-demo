b3f2dc926b7e9e13c46b51f9f2da6ff8822e027d
Clayton Salem <claytonjsalem@gmail.com>
2019-06-20 11:29:34 -0400

    A little copy

--- blocker.js
+++ blocker.js
@@ -1,7 +1,11 @@
 function blocker(message) {
     const endTime = Date.now() + 2000;
     while (Date.now() < endTime) {
-        console.log("running on main thread")
+        console.log("running blocking script")
+        // The message gets returned, but the loggin never stops in the worker, I'm not sure why.
+        if (Date.now() > endTime) {
+            break;
+        }
     }
     return message;
 
