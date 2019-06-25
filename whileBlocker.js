29350572e996c8d3397881cd7339f63c52a2bd4e
Clayton Salem <claytonjsalem@gmail.com>
2019-06-25 15:12:45 -0400

    Remove console log

--- whileBlocker.js
+++ whileBlocker.js
@@ -1,7 +1,7 @@
 function whileBlocker(message) {
     const endTime = Date.now() + 2000;
     while (Date.now() < endTime) {
-        console.log("running blocking script")
+
         // The message gets returned, but the loggin never stops in the worker, I'm not sure why.
         if (Date.now() > endTime) {
             break;
